444d0d252af146b91625a24b76c9d0e62d6c4b6d
Collin Smith <cmsmith.tx@gmail.com>
2019-09-16 14:51:42 -0500

    Config Firebase and hiding API keys

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
+
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: "whyspace-5d0b1.firebaseapp.com",
+    databaseURL: "https://whyspace-5d0b1.firebaseio.com",
+    projectId: "whyspace-5d0b1",
+    storageBucket: "",
+    messagingSenderId: "255342480096",
+    appId: process.env.REACT_APP_FIREBASE_ID
+  };
+
   return (
     <div className="App">
-      
+      <Home />
     </div>
   );
 }
