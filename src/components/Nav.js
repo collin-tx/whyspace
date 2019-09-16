e9ee1fa5d0b1a692042c80ab0501675e7866cfa6
Collin Smith <cmsmith.tx@gmail.com>
2019-09-16 17:57:59 -0500

    styled nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 export class Nav extends Component {
     render() {
         return (
-            <div>
-                <Link to="/">home</Link>
+            <div id="nav">
+                <Link to="/" className="p-1">home</Link>
                 <Link to="/explore">explore</Link>
             </div>
         )
