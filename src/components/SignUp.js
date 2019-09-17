dc8b1d1e253274cd2956183fb57f7131a89d6a6e
Collin Smith <cmsmith.tx@gmail.com>
2019-09-17 18:16:27 -0500

    added sign up form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faFacebook } from '@fortawesome/free-solid-svg-icons';
 
 const SignUp = () => {
     return (
-        <div>
-            <form>
-                <div>
-                    email: <input type="text" />
-                    password: <input type="password" />
-                    confirm password: <input type="password" />
-                </div>
-            </form>
-        </div>
+            <div className="form-container sign-up-container">
+                <form action="#">
+                    <h1>Create Account</h1>
+                    <div className="social-container">
+                    </div>
+                    <div className="row">
+                        <p>display name: </p>
+                        <input type="text" placeholder="Name" />
+                    </div>
+                    <div className="row">
+                        <p>email: </p>
+                        <input type="email" placeholder="Email" />
+                    </div>
+                    <div className="row">
+                        <p>password: </p>
+                        <input type="password" placeholder="Password" />
+                    </div>
+                    
+                    <button>Sign Up</button>
+                </form>
+            </div>
     );
 }
 
