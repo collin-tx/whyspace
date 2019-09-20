73d430fbe9bd8e52ba6def90d7f5b39a56624103
Collin Smith <cmsmith.tx@gmail.com>
2019-09-20 14:30:56 -0500

    auth create new user is working

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -5,7 +5,8 @@ export class SignUp extends React.Component {
     state = {
         displayName: '',
         email: '',
-        password: ''
+        password: '',
+        accountCreated: false
     }
 
     handleName = (e) => {
@@ -22,45 +23,59 @@ export class SignUp extends React.Component {
 
     createNewAccount = (e) => {
         e.preventDefault();
-        console.log(this.props.firebase.auth()); //getting error that firebase.auth is not a function *eyeroll*, problem was that I was only passing the database and not entire fb obj in APP
-        // this.props.firebase
-        //     .auth()
-        //     .createUserWithEmailAndPassword(this.state.email, this.state.password)
-        //     .then(user => {
-        //         console.log(user);
-        //     })
+        this.props.firebase
+            .auth()
+            .createUserWithEmailAndPassword(this.state.email, this.state.password)
+            .then(user => {
+                this.setState({ accountCreated: true })
+                // user.updateProfile({
+                //     displayName: this.state.displayName,
+                //     photoURL: `https://promiseplace.org/wp-content/uploads/2018/10/avatars-img3.jpg`
+                // });
+                console.log(user);
+            });
     }
     
     render(){
         return (
             <div className="form-container sign-up-container">
-                <form onSubmit={this.createNewAccount}>
-                    <h2>Create Account</h2>
-                    <button onClick={this.props.showSignIn}>or sign in</button>
-                    <div className="row">
-                        <p>display name: </p>
-                        <input type="text" placeholder="Name" 
-                            onChange={this.handleName} 
-                            value = {this.state.displayName}
-                             />
+                
+                {this.state.accountCreated && 
+                    <div>
+                        <h2>Account created!</h2>
+                        <button onClick={this.props.showSignIn}>sign in</button>
                     </div>
-                    <div className="row">
-                        <p>email: </p>
-                        <input type="email" placeholder="Email"
-                            onChange={this.handleEmail}
-                            value={this.state.email}
-                             />
-                    </div>
-                    <div className="row">
-                        <p>password: </p>
-                        <input type="password" placeholder="Password"
-                            onChange={this.handlePass}
-                            value={this.state.password}
-                             />
-                    </div>
-                    
-                    <button type="submit">Sign Up</button>
-                </form>
+                }
+
+                {!this.state.accountCreated &&
+                    <form onSubmit={this.createNewAccount}>
+                        <h2>Create Account</h2>
+                        <button onClick={this.props.showSignIn}>or sign in</button>
+                        <div className="row">
+                            <p>display name: </p>
+                            <input type="text" placeholder="Name" 
+                                onChange={this.handleName} 
+                                value = {this.state.displayName}
+                                />
+                        </div>
+                        <div className="row">
+                            <p>email: </p>
+                            <input type="email" placeholder="Email"
+                                onChange={this.handleEmail}
+                                value={this.state.email}
+                                />
+                        </div>
+                        <div className="row">
+                            <p>password: </p>
+                            <input type="password" placeholder="Password"
+                                onChange={this.handlePass}
+                                value={this.state.password}
+                                />
+                        </div>
+                        
+                        <button type="submit">Sign Up</button>
+                    </form>
+                }
             </div>
     );
     }
